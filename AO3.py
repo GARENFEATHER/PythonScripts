6b64f69cf802749426c9f98685deb5ca319d4886
SASUKE <garen@localhost.localdomain>
2017-02-21 09:33:57 -0800

    AO3 test finished

--- AO3.py
+++ AO3.py
@@ -11,18 +11,25 @@ def getAndSaveContent(content,fname,mode):
 	chapters=content.findAll("div",{"id":"chapters"})
 	chapters=chapters[0].findAll("div",{"class":"chapter"})
 	if len(chapters) == 0: return None
-	i=0
-	for chapter in chapters:
+	for i in range(0,len(chapters)):
+		chapter=chapters[i]
 		chapterTitle=chapter.h3.text
 		articles=chapter.findAll("div",{"role":"article"})
 		params=articles[0].findAll("p")
 		if len(params) > 0:
-			paramsText=[x.text for x in params]
-			content='\r\n'.join(paramsText)
-			txtFile.writeline(chapterTitle)
-			txtFile.writeline(content+"\r\n")
-			print "chapter",i,len(content)
-		i+=1
+			for x in xrange(0,len(params)):
+				testBrTag=params[x].findAll(text=True)
+				if len(testBrTag) != 0:
+					contents=''.join(testBrTag)
+					params[x].setString(contents)
+			paramsText=[x.text.strip() for x in params]
+			content='\r\n\r\n'.join(paramsText)
+			txtFile.write("\r\n")
+			txtFile.write(chapterTitle)
+			txtFile.write("\r\n")
+			txtFile.write(content+"\r\n")
+			txtFile.write("\r\n")
+			print "chapter-"+str(i+1),"words:",len(content)
 	txtFile.close()
 
 def systemSet():
@@ -31,6 +38,7 @@ def systemSet():
 	# os.chdir('sdcard')
 	# os.chdir('Download')
 	# os.chdir('txt')
+	# os.chdir('engNovels')
 
 def crawArticle(url,txtName=None):
 	i=0
@@ -38,28 +46,52 @@ def crawArticle(url,txtName=None):
 		try:
 			session=requests.Session()
 			data={"view_adult":True}
-			session.get(url,data)
+			session.get(url,data=data)
 			data={"view_full_work":True}
-			response=session.get(url,data)
+			response=session.get(url,data=data)
 			html=response.text
-		except ChunkedEncodingError, e:
+			break
+		except requests.exceptions.ChunkedEncodingError, e:
 			i=i+1
+			print "ChunkedEncodingError Reconnect:",i
 			if i>10:
 				print "Bad Connection"
 				return
 			continue
-		break
+		except requests.exceptions.ConnectionError, e:
+			i=i+1
+			print "ConnectionError Reconnect:",i
+			if i>10:
+				print "Bad Connection"
+				return
+			continue
 	content=b.BeautifulSoup(html)
+	session.close()
 	prefix=os.getcwd()+'/'
-	title=html.findAll("h2",{"class":"title heading"})
+	title=content.findAll("h2",{"class":"title heading"})
 	title=title[0].text
-	author=html.findAll("h3",{"class":"byline heading"})
+	author=content.findAll("h3",{"class":"byline heading"})
 	author=author[0].a.text
 	head=title+" - "+author
 	if txtName == None:
-		getAndSaveContent(html, head, None)
+		getAndSaveContent(content, head, None)
 	else:
-		getAndSaveContent(html, txtName, 'w')
+		getAndSaveContent(content, txtName, 'w')
 
 systemSet()
 url=argv[1]
+listMessage="ExistedFile: \n"
+ExList=os.walk("./").next()[2]
+ExList.sort(lambda x,y:cmp(os.path.getmtime(x),os.path.getmtime(y)), reverse=True)
+i=0
+for f in ExList:
+	listMessage=listMessage+str(i)+" "+f+"\n"
+	i=i+1
+	if i > 15: break
+num=input(listMessage)
+if num == -1 or num >= len(ExList):
+	txt=None
+else:
+	txt=ExList[num]
+
+crawArticle(url,txt)
